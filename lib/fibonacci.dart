e9503f927da12d97b6357f55eba2e23f33284536
Flávio Melo <hastnumber1@gmail.com>
2022-10-16 15:19:51 -0300

    melhoramento do print

--- lib/fibonacci.dart
+++ lib/fibonacci.dart
@@ -1,17 +1,17 @@
 //Sequência de Fibonacci
 
-main() {
-  List fib = [];
-  for (int i = 0; i <= 20; i++) {
-    fib += [fibonacci(i)];
+void main() {
+  String result = '';
+  for (int i = 0; i <= 6; i++) {
+    result += '${fibonacci(i)}, ';
   }
-  print('Desafio Masterclass - Fibonacci:');
-  print(fib);
+  print('A sequência Fibonacci é: $result...');
+  print('O valor Fibonacci é: ${fibonacci(6)}.');
 }
 
-int fibonacci(int n) {
-  if (n < 2) {
+fibonacci(int n) {
+  if (n == 0 || n == 1) {
     return n;
   }
-  return fibonacci(n - 2) + fibonacci(n - 1);
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
